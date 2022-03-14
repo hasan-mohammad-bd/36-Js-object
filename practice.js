1ef93f2048fe9eecb4c7b5db4ca30d8e8cb9bead
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-14 07:10:28 +0600

    object method

--- practice.js
+++ practice.js
@@ -21,3 +21,37 @@ class People {
         this.age = age;
     }
 }
+
+const people3 = new People("Manus", 12);
+console.log(people3);
+
+
+//object method.
+//====================================
+const student = {
+    name: "Hasan",
+    money: 5000,
+    major: "math",
+    hobby: "cycling",
+    friends: {
+        name: "San",
+        subject: "math",
+        money: 5000,
+        hobby: "swimming"
+    },
+    takeExam: function(){
+        console.log(this.name, "taking exam");
+    },
+    treatDay : function(expense, baksis){
+        this.money = this.money - expense - baksis;
+        return this.money;
+    }
+    
+}
+
+student.takeExam();
+
+const remaining = student.treatDay(900,100);
+const remaining2 = student.treatDay(900,100);
+console.log(remaining);
+console.log(remaining2);
